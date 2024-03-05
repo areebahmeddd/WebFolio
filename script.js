26692ed5d48d7401cde8b8c5bd54c29257326ac7
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-03-06 00:53:12 +0530

    v1.0

---
+++ script.js
@@ -0,0 +1,211 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menuIcon");
+    const navbarList = document.querySelector(".navbar ul");
+    const jobTitleElement = document.getElementById("jobTitle");
+    const underConstructionElement = document.getElementById("underConstruction");
+
+    const jobTitles = ["Software Engineer", "Full-Stack Developer"];
+    let currentIndex = 0;
+    let glitchInterval;
+
+    menuIcon.addEventListener("click", function () {
+        navbarList.classList.toggle("show");
+        menuIcon.classList.toggle("open");
+    });
+
+    function updateJobTitle() {
+        const currentJobTitle = jobTitles[currentIndex];
+        const typingSpeed = 100;
+
+        for (let i = 0; i <= currentJobTitle.length; i++) {
+            setTimeout(function () {
+                jobTitleElement.innerText = currentJobTitle.slice(0, i);
+            }, i * typingSpeed);
+        }
+
+        setTimeout(function () {
+            for (let i = currentJobTitle.length; i >= 0; i--) {
+                setTimeout(function () {
+                    jobTitleElement.innerText = currentJobTitle.slice(0, i);
+                }, (currentJobTitle.length - i) * typingSpeed);
+            }
+
+            currentIndex = (currentIndex + 1) % jobTitles.length;
+            setTimeout(updateJobTitle, currentJobTitle.length * typingSpeed);
+        }, currentJobTitle.length * typingSpeed + 2000);
+    }
+
+    function glitchEffect() {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
+
+        function doGlitch() {
+            let glitchedText = underConstructionElement.innerText;
+            const randomIndex = Math.floor(Math.random() * glitchedText.length);
+            glitchedText =
+                glitchedText.substring(0, randomIndex) +
+                characters[Math.floor(Math.random() * characters.length)] +
+                glitchedText.substring(randomIndex + 1);
+            underConstructionElement.innerText = glitchedText;
+        }
+
+        function reverseGlitch() {
+            let originalText = "Website is currently under construction...";
+            let glitchedText = underConstructionElement.innerText;
+            let currentIndex = glitchedText.length;
+
+            const reverseInterval = setInterval(function () {
+                if (currentIndex >= 0) {
+                    glitchedText = originalText.substring(0, currentIndex) +
+                        characters[Math.floor(Math.random() * characters.length)] +
+                        originalText.substring(currentIndex + 1);
+                    underConstructionElement.innerText = glitchedText;
+                    currentIndex--;
+                } else {
+                    clearInterval(reverseInterval);
+                    setTimeout(startNextCycle, 2000);
+                }
+            }, 100);
+        }
+
+        function stopGlitch() {
+            clearInterval(glitchInterval);
+            reverseGlitch();
+        }
+
+        function startNextCycle() {
+            glitchInterval = setInterval(function () {
+                doGlitch();
+            }, 25);
+            setTimeout(function () {
+                stopGlitch();
+            }, 5000);
+        }
+
+        setTimeout(function () {
+            startNextCycle();
+        }, 2000);
+    }
+
+    function initializeParticles() {
+        particlesJS('particles-js', {
+            "particles": {
+                "number": {
+                    "value": 80,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 5,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true,
+            "config_demo": {
+                "hide_card": false,
+                "background_color": "#b61924",
+                "background_image": "",
+                "background_position": "50% 50%",
+                "background_repeat": "no-repeat",
+                "background_size": "cover"
+            }
+        });
+    }
+
+    initializeParticles();
+    updateJobTitle();
+    glitchEffect();
+});
